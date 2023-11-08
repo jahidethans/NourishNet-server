bbe5e08978d907591beb5a8785560b26de39b89d
Jahid Hasan <jahidjlp112233@gmail.com>
2023-11-08 13:03:26 +0600

    each food found

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app =  express();
 const port = process.env.PORT || 5000;
@@ -26,12 +26,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db('nourishNet').collection('allfoods')
+
+
+    app.get('/allfoods', async(req, res)=>{
+        const cursor = foodCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/allfoods/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
