7d1dcb27ebe90b0555c81b1f54efc6e0dc18ba0a
Jahid Hasan <jahidjlp112233@gmail.com>
2023-11-08 15:52:22 +0600

    add a food completed

--- index.js
+++ index.js
@@ -29,17 +29,24 @@ async function run() {
 
     const foodCollection = client.db('nourishNet').collection('allfoods')
 
-
+    // show all available foods
     app.get('/allfoods', async(req, res)=>{
         const cursor = foodCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
 
+    // show specific food detail
     app.get('/allfoods/:id', async(req, res)=>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id)}
       const result = await foodCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/allfoods', async(req, res)=>{
+      const newFood = req.body;
+      const result = await foodCollection.insertOne(newFood);
       res.send(result);
     })
 
