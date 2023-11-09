1ad23f8c8298b1c48690fdc93e7567ff9fc3ef23
Jahid Hasan <jahidjlp112233@gmail.com>
2023-11-09 17:15:37 +0600

    backend completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const app =  express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -31,88 +31,113 @@ async function run() {
     const requestCollection = client.db('nourishNet').collection('requests')
 
     // show all available foods
-    app.get('/allfoods', async(req, res)=>{
-        const cursor = foodCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/allfoods', async (req, res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
     // show all requested foods
-    app.get('/allrequests', async(req, res)=>{
-        const cursor = requestCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/allrequests', async (req, res) => {
+      const cursor = requestCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     // get one requested food by id
-    app.get('/allrequests/:id', async(req, res)=>{
+    app.get('/allrequests/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await requestCollection.findOne(query);
       res.send(result)
     })
 
-   // show user-based requested foods
-app.get('/ownrequests', async (req, res) => {
-  let query = {};
-  if (req.query?.requestorEmail) {
-    query = { requestorEmail: req.query.requestorEmail};
-  }
-  const result = await requestCollection.find(query).toArray();
-  res.send(result);
-});
-
-
-   // show specific food detail for manage data
-   app.get('/allrequests/foods/:foodId', async (req, res) => {
-    const foodId = req.params.foodId;
-    console.log(foodId);
-    const query = { foodId }; // Use foodId to query for the specific food
-    const result = await requestCollection.find(query).toArray();
-    
-    
+    // show user-based requested foods
+    app.get('/ownrequests', async (req, res) => {
+      let query = {};
+      if (req.query?.requestorEmail) {
+        query = { requestorEmail: req.query.requestorEmail };
+      }
+      const result = await requestCollection.find(query).toArray();
       res.send(result);
-    
-  });
+    });
+
+
+    // show specific food detail for manage data
+    app.get('/allrequests/foods/:foodId', async (req, res) => {
+      const foodId = req.params.foodId;
+      console.log(foodId);
+      const query = { foodId }; // Use foodId to query for the specific food
+      const result = await requestCollection.find(query).toArray();
+
+
+      res.send(result);
+
+    });
+
+    // food updated by specific user
+    app.put('/allfoods/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedFood = req.body;
+      const food = {
+        $set: {
+         
+        foodImage: updatedFood.foodImage,
+        foodName: updatedFood.foodName,
+        donatorName: updatedFood.donatorName,
+        donatorImage: updatedFood.donatorImage,
+        foodQuantity: updatedFood.foodQuantity,
+        pickupLocation: updatedFood.pickupLocation,
+        expiredDate: updatedFood.expiredDate,
+        additionalNotes: updatedFood.additionalNotes,
+        email: updatedFood.email,
+        foodStatus: updatedFood.foodStatus,
+        }
+      }
+      const result = await foodCollection.updateOne(filter, food, options );
+      res.send(result);
+    })
 
 
 
 
     // show specific food detail
-    app.get('/allfoods/:id', async(req, res)=>{
+    app.get('/allfoods/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await foodCollection.findOne(query);
       res.send(result);
     })
 
     // get user based food for manage my food page
-    app.get('/managefoods', async(req, res)=>{
+    app.get('/managefoods', async (req, res) => {
       let query = {};
-      if(req.query?.email){
-        query = {email: req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email }
       }
       const result = await foodCollection.find(query).toArray();
       res.send(result)
     })
 
     // add one food
-    app.post('/allfoods', async(req, res)=>{
+    app.post('/allfoods', async (req, res) => {
       const newFood = req.body;
       const result = await foodCollection.insertOne(newFood);
       res.send(result);
     })
-    
+
     // add one request
-    app.post('/allrequests', async(req, res)=>{
+    app.post('/allrequests', async (req, res) => {
       const newFood = req.body;
       const result = await requestCollection.insertOne(newFood);
       res.send(result);
     })
 
     // update status from manage request page
-    app.patch('/allrequests/:id', async(req, res)=>{
+    app.patch('/allrequests/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updatedStatusOfFood = req.body;
       console.log(updatedStatusOfFood);
       const updatedDoc = {
@@ -132,9 +157,17 @@ app.get('/ownrequests', async (req, res) => {
       const query = { _id: new ObjectId(id) };
       const result = await requestCollection.deleteOne(query);
       res.send(result);
-      
+
     });
-    
+
+    // delete by user from manage their food page
+    app.delete('/allfoods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
@@ -149,10 +182,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('food is running')
+app.get('/', (req, res) => {
+  res.send('food is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`nourish server is running on ${port}` );
+app.listen(port, () => {
+  console.log(`nourish server is running on ${port}`);
 })
